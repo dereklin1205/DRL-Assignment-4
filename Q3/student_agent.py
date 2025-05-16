25e40aaf0d00450b7e4196ae49286821d6cf6d41
dereklin1205 <dereklin1205@gmail.com>
2025-05-16 13:49:44 +0800

    all_re

--- Q3/student_agent.py
+++ Q3/student_agent.py
@@ -6,7 +6,11 @@ class Agent(object):
     """Agent that acts randomly."""
     def __init__(self):
         self.action_space = gym.spaces.Box(-1.0, 1.0, (21,), np.float64)
-        self.agent = SACAgent()
+        self.state_dim = 67
+        self.action_dim = 21
+        self.action_space_low = -1.0
+        self.action_space_high = 1.0
+        self.agent = SACAgent(self.state_dim, self.action_dim, self.action_space_low, self.action_space_high, 256)
         self.agent.load("./sac_humanoid_models")
         self.agent.eval()
     def act(self, observation):
