1f26a0181462e9e7ea7d2eb0c3f0c29ec5728d72
dereklin1205 <dereklin1205@gmail.com>
2025-05-16 13:56:07 +0800

    fixstu

--- Q3/student_agent.py
+++ Q3/student_agent.py
@@ -20,6 +20,11 @@ from dmc import make_dmc_env
 from train import SACAgent
 # Do not modify the input of the 'act' function and the '__init__' function. 
 RANDOM_SEED = 42
+def make_env():
+    env_name = "humanoid-walk"
+    env = make_dmc_env(env_name, np.random.randint(0, 1000000), flatten=True, use_pixels=False)
+    return env
+
 class Agent(object):
     """Agent that acts randomly."""
     def __init__(self):
